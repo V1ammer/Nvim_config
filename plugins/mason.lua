0f932b3a384ff64bd101a0273fc299a991e837f5
V1ammer <v1lammer@gmail.com>
2023-06-07 18:38:39 +0300

    Add greeting and tabnine plugin

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,7 +7,12 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "lua_ls",
+        "eslint",
+        "pyright",
+        "rust_analyzer",
+        "tailwindcss",
+        "tsserver",
       })
     end,
   },
@@ -18,8 +23,11 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
+        "stylua",
+        "prettierd",
+        "pylint",
+        "mypy",
+        "rustfmt",
       })
     end,
   },
@@ -29,7 +37,9 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
+        "debugpy",
+        "cpptools",
+        "codelldb",
       })
     end,
   },
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,19 @@
 return {
+  {
+  "codota/tabnine-nvim",
+  build = "./dl_binaries.sh",
+  event = "VeryLazy",
+  config = function()
+    require("tabnine").setup {
+      disable_auto_comment = true,
+      accept_keymap = "<A-x>",
+      dismiss_keymap = "<C-]>",
+      debounce_ms = 800,
+      suggestion_color = { gui = "#808080", cterm = 244 },
+      exclude_filetypes = { "TelescopePrompt" },
+    }
+  end,
+},
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
